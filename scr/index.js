68bf447f897aee26b6167bb68f41d1d6c78182d3
Klaudia Witman <claudiawitman@gmail.com>
2024-09-25 14:30:41 +0100

    added axios and another fcking to finalise the answer with typewritter type.

--- scr/index.js
+++ scr/index.js
@@ -1,12 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
   new Typewriter("#recipe-paragraph", {
-    strings: ["Hello World"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionelement = document.querySelector("#searchBar");
+  let apiKey = `9e6b9ffc064obdabfd0f8416t935a1a9`;
+  let context =
+    "You are one of the greatest chefs in the world. You are concise with your recipes and generate them in basic html.";
+  let prompt = `Generate a ${instructionelement.value} recipe`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#form-recipe-generator");

59e6e0a8cd6c4feb89dab9b31a0015eb9aa27daa
Klaudia Witman <claudiawitman@gmail.com>
2024-09-25 14:40:20 +0100

    fianl conetxt changes

--- scr/index.js
+++ scr/index.js
@@ -13,7 +13,7 @@ function generateRecipe(event) {
   let instructionelement = document.querySelector("#searchBar");
   let apiKey = `9e6b9ffc064obdabfd0f8416t935a1a9`;
   let context =
-    "You are one of the greatest chefs in the world. You are concise with your recipes and generate them in basic html.";
+    "You are one of the greatest chefs in the world. You are concise with your recipes and generate them in basic html engining with something polite and sign with <stong> Klaudia</strong>";
   let prompt = `Generate a ${instructionelement.value} recipe`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 

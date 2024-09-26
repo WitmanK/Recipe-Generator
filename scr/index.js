45ecb40491abd8cee65cf91731816078e68a5f7f
Klaudia Witman <claudiawitman@gmail.com>
2024-09-26 09:59:13 +0100

    adding further CSS and JS

--- scr/index.js
+++ scr/index.js
@@ -13,9 +13,13 @@ function generateRecipe(event) {
   let instructionelement = document.querySelector("#searchBar");
   let apiKey = `9e6b9ffc064obdabfd0f8416t935a1a9`;
   let context =
-    "You are one of the greatest chefs in the world. You are concise with your recipes and generate them in basic html engining with something polite and sign with <stong> Klaudia</strong>";
+    "You are one of the greatest chefs in the world. You are concise with your recipes and generate them in basic html, without the ```html and ``` at the start and end of the recipe. Can you also please end all recipes with something polite and sign with <stong> Klaudia</strong>";
   let prompt = `Generate a ${instructionelement.value} recipe`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe-paragraph");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">Generating a ${instructionelement.value} recipe...</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
